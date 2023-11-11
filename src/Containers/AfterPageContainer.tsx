3b2ea6f094c458a4198c121346d6c7c0b625ab78
omer_sm <smorodinskyomer@gmail.com>
2023-11-11 12:56:48 +0200

    fixed blood splatter and favicon

--- src/Containers/AfterPageContainer.tsx
+++ src/Containers/AfterPageContainer.tsx
@@ -25,6 +25,8 @@ export default function AfterPageContainer({ transitionStage, isDesktopLayout,
             }
             if (navigator.canShare(shareData) && !isDesktopLayout) {
                 navigator.share(shareData)
+            } else {
+                setIsShareTooltipVisible((p: boolean) => !p)
             }
         } else {
             setIsShareTooltipVisible((p: boolean) => !p)
